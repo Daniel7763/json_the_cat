a4b62fcdc64e23da3e554edc8c4fe00b457fd8c7
Daniel Soares <danielsoares01@outlook.com>
2023-02-28 21:26:57 -0800

    jason the cat test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,25 @@
+// Require Request Module
 const request = require("request");
-//const fs = require("fs");
+// Require File System module
 
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-//const URL = process.argv[2];
-const breed = process.argv[2];
-//const Filepath = process.argv[3];
+const fetchBreedDescription = (breed, callback) => {
+  request(url + breed, (error, response, body) => {
+    if (error) {
+      callback(error);
+      // console.log("error:", error);
+    } else if (body) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null);
+        // console.log("Breed not found");
+      } else {
+        callback(null, data[0].description);
+        // console.log(data[0].description);
+      }
+    }
+  });
+};
 
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`,(error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  }
-
-  const data = JSON.parse(body);
-  //console.log(data);
-  if (data.length === 0) {
-    console.log(`Breed is not in database.`);
-    return;
-  }
-
-  console.log(`Showing info about the ${breed} breed:`, data[0].description);
-
-});
+module.exports = { fetchBreedDescription };
